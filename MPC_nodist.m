9044869fee6fb0431820b96018617d074eb9fe50
vgoritud <v.gori@student.tudelft.nl>
2024-06-15 14:41:15 +0200

    working MPC with Xf

--- MPC_nodist.m
+++ MPC_nodist.m
@@ -2,9 +2,9 @@ clear all
 close all
 clc
 %%
-% INSPIRATION TAKEN FROM MPC CODE 
-load('b_in_Tset.mat')
-load('A_in_Tset.mat')
+% % INSPIRATION TAKEN FROM MPC CODE 
+% load('b_in_Tset.mat')
+% load('A_in_Tset.mat')
 
 %%
 % INSPIRATION TAKEN FROM MPC CODE 
@@ -35,19 +35,19 @@ G_disc.C = sys_DT.C;
 G_disc.D = sys_DT.D;
 
 
-weight.Q =[4.5 0 0;0 3.5 0; 0 0 0.0071];   % weight on output
-weight.R = 20;
+weight.Q =[5 0 0;0 4.5 0; 0 0 0.0071];   % weight on output
+weight.R = 30;
 [P, K, eigvals] = dare(G_disc.A, G_disc.B, weight.Q, weight.R, [], []);
 weight.P = P;
 % R = 10, N = 40 works, otherise terminal ste is a bit not converged
 % Simulation horizon and input constraints
-T_sim = 100;
-umin = -0.11; % 0.1 A as rated current
+T_sim = 500;
+umin = -4; % 0.1 A as rated current
 umax = -umin;  
 dim.nx = size(G_disc.A,1); % state dimension
 dim.nu = size(G_disc.B,2); % input dimension
 dim.ny = size(G_disc.C,1);
-dim.N = 40;
+dim.N = 20;
 
 % Define prediction model + cost
 [S,T] = predmodgen(G_disc,dim);
@@ -82,46 +82,80 @@ A_in = F*S;
 % Equality constraints (terminal)
 Ae = S(end-dim.nx+1:end,:);
 be = T(end-dim.nx+1:end,:);
-
+ % Compute terminal set using MPT3
+model = LTISystem(sys_DT);
+model.x.min = -[x1_constraint; x2_constraint; x3_constraint];
+model.x.max = [x1_constraint; x2_constraint; x3_constraint];
+model.u.max = umax;
+model.u.min = umin;
+model.x.penalty = QuadFunction(weight.Q);
+model.u.penalty = QuadFunction(weight.R);
+Tset = model.LQRSet;
+% PN = model.LQRPenalty;
+% model.x.with('terminalSet');
+% model.x.terminalSet = Tset;
+% model.x.with('terminalPenalty');
+% model.x.terminalPenalty = PN;
+% % dim.N = 50;
+% ctrl = MPCController(model, dim.N)
+% x0 = [0.1; 0; 0];
+% Nsim = 500;
+% loop = ClosedLoop(ctrl, model);
+% data = loop.simulate(x0, Nsim);
+% figure(1)
+% subplot(2,1,1)
+% plot(1:Nsim, data.Y);
+% hold on;
+% title('outputs')
+% subplot(2,1,2)
+% plot(1:Nsim, data.U);
+% title('inputs')
 
 %% MPC
 total_iterations = T_sim;
 o = waitbar(0, 'Progress');
 for k = 1 : T_sim
     Constraint = [];
-    x_0 = x(:,k); 
+    x_0 = x(:,k); s
     u_horizon = sdpvar(dim.nu*dim.N,1);  % [5,1] where 5 is horizon, 1, input dim          
     % MPC using quadprog
     
-    A_in = [A_in]%; A_in_Tset*Ae];
-    b_in = [eps - eps_1*x_0]%; (b_in_Tset - A_in_Tset*be*x_0 )];
+    A_in_QP = [A_in; Tset.A*Ae];
+    b_in_QP = [(eps-eps_1*x_0); (Tset.b - Tset.A*be*x_0)];
     lb = umin*ones(dim.N,1);
     ub = umax*ones(dim.N,1);
-
-    u_horizon = quadprog(H, (h * x_0), A_in, b_in, [],[], lb, ub )
+    [u_horizon_QP,fval,exitflag,output]= quadprog(H, (h * x_0), A_in_QP, b_in_QP, [],[], lb, ub );
+    exitflag
     % Contraints
 %     Constraint = [Constraint, 
 %                     umin <= u_horizon <= umax,
 %                     A_in * u_horizon <= eps - eps_1*x_0,
-%                     Tset.A*(Ae * u_horizon + be * x_0) <= Tset.b,
+%                     Tset.A * Ae * u_horizon <= Tset.b - Tset.A*be*x_0,
 %                     ];                                        %define constraints
-    
-    % Objective function
-%     Objective = u_horizon' * H * u_horizon + (h * x_0)' * u_horizon;     
 %     
-%     % Optimize
+    % A_in has size 126 x 20
+    % Tset.A * Ae  has size 34 x 20 --> the augmented A has size 160, 20
+    % size(eps - eps_1*x_0) = 126 x 1
+    % size(Tset.b - Tset.A*be*x_0) = 34 x 1 -- > the augmented version = 160 ,1
+
+
+    % Objective function
+
+%     Objective = u_horizon' * H * u_horizon + (h * x_0)' * u_horizon;     
+    
+    % Optimize
 %     ops = sdpsettings('verbose', 0, 'solver', 'quadprog');  % Specify 'quadprog' as the solver
-% %     ops =  sdpsettings('verbose',0);
-%     sol = optimize(Constraint,Objective, ops);                 %solve the problem
+%     ops =  sdpsettings('verbose',0);
+% %     sol = optimize(Constraint,Objective, ops);                 %solve the problem
     
 %     if sol.problem == 0
 %         u_horizon = value(u_horizon);                          %assign the solution
-%         % Select the first input only
-        u_rec(k)=u_horizon(1);
+        % Select the first input only
+        u_rec(k)=u_horizon_QP(1);
         % Compute the state/output evolution
         x(:,k+1)=G_disc.A*x_0 + G_disc.B*u_rec(k);        
 
-        clear u_horizon
+%         clear u_horizon
 %     else
 %         disp('Problem solving MPC optimization');
 %         break
